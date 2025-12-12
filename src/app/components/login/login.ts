b6da1c79956213ab97b384b80936b1071b47dff5
Santiago2105 <u202210810@upc.edu.pe>
2025-12-12 16:44:42 -0500

    UltimaVersion

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { UserDTO } from '../../models/userDTO';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user-service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class Login {
 loginForm!:FormGroup;
 
-  constructor (private userService:UserService,  private formBuilder: FormBuilder,
+  constructor (private userService:UserService,  private formBuilder: FormBuilder,private _snackBar: MatSnackBar,
               private router:Router
   ){}
 
@@ -21,27 +22,51 @@ loginForm!:FormGroup;
     this.CargarFormulario();
   }
 
-  CargarFormulario(){
-    this.loginForm = this.formBuilder.group(
-      {
-        username:[""],
-        password:[""]
-      }
-    );
+CargarFormulario() {
+    this.loginForm = this.formBuilder.group({
+      // 4. Agregamos Validators.required a los campos
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
-  Ingresar(){
-    const userDTO: UserDTO={
-          id: 0,
-          username: this.loginForm.get("username")?.value,
-          password: this.loginForm.get("password")?.value,
-          authorities: ""
+  Ingresar() {
+    // 5. Validamos antes de enviar
+    if (this.loginForm.invalid) {
+      // 1. Validamos si el formulario es inválido
+    if (this.loginForm.invalid) {
+      
+      // TRUCO: Marcamos todos los campos como "tocados" para que los mat-error se pinten de rojo
+      this.loginForm.markAllAsTouched(); 
+      
+      // 2. Mostramos el SnackBar
+      this._snackBar.open('Por favor, complete todos los campos obligatorios', 'Cerrar', {
+        duration: 3000,
+        panelClass: ['warn-snackbar'], // Asegura que tenga estilo si usas clases personalizadas
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center'
+      });
+      return; // Detenemos la ejecución
+    }
     }
 
+    const userDTO: UserDTO = {
+      id: 0,
+      username: this.loginForm.get('username')?.value,
+      password: this.loginForm.get('password')?.value,
+      authorities: '',
+    };
+
     this.userService.login(userDTO).subscribe({
-      next:()=>{
-        this.router.navigate(["/home"]);
+      next: () => {
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        // 6. Bonus: Mostrar mensaje si las credenciales son incorrectas (backend rechaza)
+        this._snackBar.open('Usuario o contraseña incorrectos', 'Error', {
+          duration: 3000,
+        });
       }
-    })
+    });
   }
 }
